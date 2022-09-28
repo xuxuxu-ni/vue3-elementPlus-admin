344a0879c60576ac966f9bcf36d420187da080f4
nirongxu <595485548@qq.com>
2022-09-28 10:11:00 +0800

    监听浏览器直接输入路由，将此路由添加到tabnavBox

--- src/views/layout/aside/aside.vue
+++ src/views/layout/aside/aside.vue
@@ -39,56 +39,48 @@
   </div>
 </template>
 
-<script lang="ts">
+<script setup lang="ts">
+import { useRouter, useRoute } from 'vue-router'
 import menuTree from './menuTree.vue'
 
-import {defineComponent} from 'vue'
+import {defineComponent, watch} from 'vue'
 import {layoutStore} from '@/store/modules/layoutStore'
 import { routerDataStore } from '@/store/modules/routerDataStore'
+const route = useRoute()
 
-export default defineComponent({
-  name: 'AsideNav',
-  components: {
-    menuTree
-  },
-  setup() {
-    const layoutState = layoutStore()
-    const routerDataState = routerDataStore()
-    // 监听浏览器直接输入路由，将此路由添加到tabnavBox
-    // watch('$route.path', (newval) => {
-    //   this.selectMenu(newval)
-    // })
-
-    function selectMenu(key: string, indexpath: string) {
-      // 如果不使用 elemenUI 菜单的 vue-router 的模式将用以下方式进行页面跳转 el-menu的router设置为false
-      // this.$router.push(indexpath.join("/"))
-      console.log(indexpath)
-
-      let router = routerDataState.routers
-      let name = ''
-      let navTitle = function (path: string, routerARR: any) {
-        for (let i = 0; i < routerARR.length; i++) {
-          if (routerARR[i]?.children.length > 0 || routerARR[i].path === path) {
-            if (routerARR[i].path === path && routerARR[i].children.length < 1) {
-              name = routerARR[i].name
-              break
-            }
-            navTitle(path, routerARR[i].children)
-          }
-        }
-        return name
-      }
-
-      layoutState.addTab({
-        title: navTitle(key, router),
-        path: key
-      })
-
-    }
-
-    return {selectMenu, layoutState, routerDataState}
-  }
+const layoutState = layoutStore()
+const routerDataState = routerDataStore()
+// 监听浏览器直接输入路由，将此路由添加到tabnavBox
+watch(() => route.path, (newval) => {
+  selectMenu(newval)
 })
+
+function selectMenu(key: string, indexpath?: string) {
+  // 如果不使用 elemenUI 菜单的 vue-router 的模式将用以下方式进行页面跳转 el-menu的router设置为false
+  // router.push(indexpath.join("/"))
+  console.log(indexpath)
+
+  let router = routerDataState.routers
+  let name = ''
+  let navTitle = function (path: string, routerARR: any) {
+    for (let i = 0; i < routerARR.length; i++) {
+      if (routerARR[i]?.children.length > 0 || routerARR[i].path === path) {
+        if (routerARR[i].path === path && routerARR[i].children.length < 1) {
+          name = routerARR[i].name
+          break
+        }
+        navTitle(path, routerARR[i].children)
+      }
+    }
+    return name
+  }
+
+  layoutState.addTab({
+    title: navTitle(key, router),
+    path: key
+  })
+
+}
 </script>
 
 <style lang="scss">
